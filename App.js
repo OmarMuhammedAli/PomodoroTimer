1604a0578de5b73857a6e5ff2c7f2cf01e7a0f30
Omar <omarmuhammed1998.om@gmail.com>
2020-10-09 12:06:39 +0200

    Modified App.js

--- App.js
+++ App.js
@@ -1,14 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import PomodoroTimer from './components/PomodoroTimer'
 
+
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      workMins: 25,
+      breakMins: 5,
+    }
+  }
+
+  handleWorkTime = (workTime) => {
+    if (workTime >= 0) {
+      this.setState({
+        workMins: workTime
+      })
+    } else {
+      alert('Invalid Value. Setting to default.')
+      this.setState({
+        workMins: 25
+      })
+    }
+    
+  }
+
+  handleBreakTime = (breakTime) => {
+    if (breakTime >= 0) {
+      this.setState({
+        breakMins: breakTime
+      })
+    } else {
+      alert('Invalid Value. Setting to default.')
+      this.setState({
+        breakMins: 5
+      })
+    }
+  }
+
   render() {
     return (
-      <PomodoroTimer />
+        <PomodoroTimer 
+        work={this.state.workMins}
+        break={this.state.breakMins}
+        handleWorkTime={this.handleWorkTime}
+        handleBreakTime={this.handleBreakTime}
+        />
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
